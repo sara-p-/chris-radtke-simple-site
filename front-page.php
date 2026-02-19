a2dc079f991a08def0a6b83aff5f851f89486703
sara-p- <sarapitt88@gmail.com>
2026-02-18 18:14:26 -0800

    Style - Front Page

--- front-page.php
+++ front-page.php
@@ -9,7 +9,10 @@
 
 <div class="container">
   <div class="content">
-    <h1 class="page-title"><?php echo get_the_title(); ?></h1>
+    <h1 class="content__title"><?php echo get_the_title(); ?></h1>
+    <div class="content__featured-image">
+      <img src="<?php echo get_the_post_thumbnail_url(null, 'large'); ?>" alt="Chris Radtke portrait">
+    </div>
     <div class="content__text">
       <?php  the_field('content'); ?>
     </div>
@@ -18,17 +21,16 @@
   <h2 class="work__title">Work</h2>
   <div class="loop">
     <?php while($workPosts->have_posts()) : $workPosts->the_post(); ?>
-    <div class="post">
-      <a href="<?php the_permalink(); ?>" class="post__card">
-        <div class="post__image">
-          <img src="<?php echo get_the_post_thumbnail_url(null, 'large' ) ?>" alt="<?php echo the_title() ?>">
-        </div>
-        <p class="post__title"><?php the_title(); ?></p>
-      </a>
-    </div>
+    <a href="<?php the_permalink(); ?>" class="post">
+      <div class="post__image">
+        <img src="<?php echo get_the_post_thumbnail_url(null, 'large' ) ?>"
+          alt="<?php echo the_title() ?> thumbnail image">
+      </div>
+      <p class="post__title"><?php the_title(); ?></p>
+    </a>
+    <!-- <div class="post">
+    </div> -->
     <?php endwhile; ?>
   </div>
   <?php endif; ?>
 </div>
-
-<?php get_footer(); ?>
